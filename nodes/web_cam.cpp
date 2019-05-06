f9d4ef7e4ca3f8525ee5638bdc299b594a811623
lsw <664307487@qq.com>
2019-05-06 16:50:04 +0800

    fullsrceen show images

--- nodes/web_cam.cpp
+++ nodes/web_cam.cpp
@@ -14,7 +14,8 @@ using namespace cv;
 
 
 image_transport::Publisher image_pub;
-
+//设置图像大小
+cv::Size image_size = Size(1920.0, 1080.0);
 
 int main(int argc, char **argv)
 {
@@ -27,7 +28,13 @@ int main(int argc, char **argv)
 
     // 用系统默认驱动读取摄像头0，使用其他摄像头ID，请在这里修改
     cv::VideoCapture cap(0);
+    // 设置摄像头分辨率
+    cap.set(CAP_PROP_FRAME_WIDTH, image_size.height);
+    cap.set(CAP_PROP_FRAME_HEIGHT, image_size.width);
     cv::Mat frame;
+    // 设置全屏
+    namedWindow("web_cam frame", CV_WINDOW_NORMAL);
+    setWindowProperty("web_cam frame", CV_WND_PROP_FULLSCREEN, CV_WINDOW_FULLSCREEN);
 
     sensor_msgs::ImagePtr msg;
     int id = 0;
@@ -39,6 +46,10 @@ int main(int argc, char **argv)
             //添加ID
             frame = IDManagement::add_id(id, frame);
             id = id+1;
+            // 改变图像大小并显示图片
+            resize(frame, frame, image_size);
+            imshow("web_cam frame", frame);
+            waitKey(5);
             // 设置图像帧格式->bgr8
             msg = cv_bridge::CvImage(std_msgs::Header(), "bgr8", frame).toImageMsg();
             // 将图像通过话题发布出去
@@ -51,4 +62,3 @@ int main(int argc, char **argv)
 
     cap.release();
 }
-
